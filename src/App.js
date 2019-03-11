d5a60fea8ee27167c6c1078838347c1ad590e0c1
henian <1043000279@qq.com>
2019-03-11 15:47:08 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import $ from "jquery"
+import axios from "axios"
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    console.log("挂载了", $)
+    axios.get("https://api.github.com/users/octocat/gists").then(res => {
+      console.log(res, 778787)
+    });
+  }
   render() {
+    console.log(this.props.abc)
+    let arr = [1,2,3,45,6,6]
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p abd="ddd">
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
@@ -19,10 +29,15 @@ class App extends Component {
           >
             Learn React
           </a>
+          {arr.join('-')}
         </header>
       </div>
     );
   }
+}
+
+App.defaultProps = {
+  abc: "abc"
 }
 
 export default App;
